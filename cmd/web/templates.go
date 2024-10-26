47c0a1c614f199ebd2cff87816265ec5a55f05af
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 17:17:12 +0300

    fix: introduced a custom template function that takes the date and turns it into human readable date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,7 +4,7 @@ package main
 import (
     "html/template" // New import
     "path/filepath" // New import
-
+	"time"
     "GoSnippetBox/internal/models"
 )
 
@@ -18,6 +18,21 @@ type templateData struct {
 	Snippets []*models.Snippet
 }
 
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup between the names of our
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+    "humanDate": humanDate,
+}
+
+
+
 func newTemplateCache() (map[string]*template.Template, error) {
     cache := map[string]*template.Template{}
 
@@ -30,8 +45,8 @@ func newTemplateCache() (map[string]*template.Template, error) {
         name := filepath.Base(page)
 
         // Parse the base template file into a template set.
-        ts, err := template.ParseFiles("./ui/html/base.tmpl")
-        if err != nil {
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		        if err != nil {
             return nil, err
         }
 
