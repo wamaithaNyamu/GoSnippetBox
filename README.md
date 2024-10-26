a6fb76d8a45fc7da289efc972a9d95e88ef8591d
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 13:41:04 +0300

    feat: Level logging for info and error

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ To run the go program on a different port  (4000 is the default)
 ```sh
 go run ./cmd/web -addr=":9999"
 ```
+
+To get automated help that shows the available command line flags use the -help flag
+
+```sh
+go run ./cmd/web -help
+```
+Getting the cmd flags from the environment
+
+```sh
+export SNIPPETBOX_ADDR=":9999"
+go run ./cmd/web -addr=$SNIPPETBOX_ADDR
+
+```
+
+If you needed to get it directly in code:
+
+```go
+addr := os.Getenv("SNIPPETBOX_ADDR")
+```
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -18,6 +19,17 @@ func main() {
     // otherwise it will always contain the default value of ":4000". If any errors are
     // encountered during parsing the application will be terminated.
     flag.Parse()
+ // Use log.New() to create a logger for writing information messages. This takes
+    // three parameters: the destination to write the logs to (os.Stdout), a string
+    // prefix for message (INFO followed by a tab), and flags to indicate what
+    // additional information to include (local date and time). Note that the flags
+    // are joined using the bitwise OR operator |.
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    
+    // Create a logger for writing error messages in the same way, but use stderr as
+    // the destination and use the log.Lshortfile flag to include the relevant
+    // file name and line number.
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
     mux := http.NewServeMux()
       // Create a file server which serves files out of the "./ui/static" directory.
@@ -34,7 +46,9 @@ func main() {
     mux.HandleFunc("/snippet/view", snippetView)
     mux.HandleFunc("/snippet/create", snippetCreate)
 
-    log.Printf("Starting server on %s" ,*addr)
+    // Write messages using the two new loggers, instead of the standard logger.
+    infoLog.Printf("Starting server on %s", *addr)
+    
     err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+    errorLog.Fatal(err)
 }
