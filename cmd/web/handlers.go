69e4ea86d9ca600f0d9bfb48c94752b8f3233183
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 13:16:06 +0300

    feat: html templating and inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +14,38 @@ func home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    w.Write([]byte("Hello from Snippetbox"))
+	 // Initialize a slice containing the paths to the two files. It's important
+    // to note that the file containing our base template must be the *first*
+    // file in the slice.
+    files := []string{
+        "./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+        "./ui/html/pages/home.tmpl",
+    }
+
+    // Use the template.ParseFiles() function to read the template file into a
+    // template set. If there's an error, we log the detailed error message and use
+    // the http.Error() function to send a generic 500 Internal Server Error
+    // response to the user.
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    // We then use the Execute() method on the template set to write the
+    // template content as the response body. The last parameter to Execute()
+    // represents any dynamic data that we want to pass in, which for now we'll
+    // leave as nil.
+
+	// Use the ExecuteTemplate() method to write the content of the "base" 
+    // template as the response body.
+    err = ts.ExecuteTemplate(w, "base",nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
