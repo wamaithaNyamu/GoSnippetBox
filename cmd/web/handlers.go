a0ef1bf71b64aca0dd0fd7298c5590230946bbb2
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 20:26:10 +0300

    feat: clean urls and method based routing with http router

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 	"strconv"
 	"GoSnippetBox/internal/models"
+	"github.com/julienschmidt/httprouter" 
 )
 
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
+ // Because httprouter matches the "/" path exactly, we can now remove the
+    // manual check of r.URL.Path != "/" from this handler.
+
 
     snippets, err := app.snippets.Latest()
     if err != nil {
@@ -32,8 +32,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
+	   // When httprouter is parsing a request, the values of any named parameters
+    // will be stored in the request context. We'll talk about request context
+    // in detail later in the book, but for now it's enough to know that you can
+    // use the ParamsFromContext() function to retrieve a slice containing these
+    // parameter names and values like so:
+    params := httprouter.ParamsFromContext(r.Context())
+
+
+ id, err := strconv.Atoi(params.ByName("id"))
+ 
+ if err != nil || id < 1 {
         app.notFound(w)
         return
     }
@@ -55,27 +64,28 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 
+// Add a new snippetCreate handler, which for now returns a placeholder
+// response. We'll update this shortly to show a HTML form.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed)
-        return
-    }
+    w.Write([]byte("Display the form for creating a new snippet..."))
+}
 
-    // Create some variables holding dummy data. We'll remove these later on
-    // during the build.
+
+// Rename this handler to snippetCreatePost.
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+    // Checking if the request method is a POST is now superfluous and can be
+    // removed, because this is done automatically by httprouter.
+
     title := "O snail"
     content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
     expires := 7
 
-    // Pass the data to the SnippetModel.Insert() method, receiving the
-    // ID of the new record back.
     id, err := app.snippets.Insert(title, content, expires)
     if err != nil {
         app.serverError(w, err)
         return
     }
 
-    // Redirect the user to the relevant page for the snippet.
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+    // Update the redirect path to use the new clean URL format.
+    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
