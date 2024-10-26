138133a67f4b0e61e34682ed12f95d11cf8260ea
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 13:51:29 +0300

    fix: using dependency injection to reuse the loggers from the main.go into the handlers.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+// Define an application struct to hold the application-wide dependencies for the
+// web application. For now we'll only include fields for the two custom loggers, but
+// we'll add more to it as the build progresses.
+type application struct {
+    errorLog *log.Logger
+    infoLog  *log.Logger
+}
+
+
 func main() {
      // Define a new command-line flag with the name 'addr', a default value of ":4000"
     // and some short help text explaining what the flag controls. The value of the
@@ -31,6 +40,13 @@ func main() {
     // file name and line number.
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+      // Initialize a new instance of our application struct, containing the
+    // dependencies.
+    app := &application{
+        errorLog: errorLog,
+        infoLog:  infoLog,
+    }
+
     mux := http.NewServeMux()
       // Create a file server which serves files out of the "./ui/static" directory.
     // Note that the path given to the http.Dir function is relative to the project
@@ -42,9 +58,9 @@ func main() {
     // "/static" prefix before the request reaches the file server.
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet/view", snippetView)
-    mux.HandleFunc("/snippet/create", snippetCreate)
+    mux.HandleFunc("/", app.home)
+    mux.HandleFunc("/snippet/view", app.snippetView)
+    mux.HandleFunc("/snippet/create", app.snippetCreate)
 // Initialize a new http.Server struct. We set the Addr and Handler fields so
     // that the server uses the same network address and routes as before, and set
     // the ErrorLog field so that the server now uses the custom errorLog logger in
