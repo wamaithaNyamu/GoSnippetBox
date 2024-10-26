6c5fa256690beb3c7863bef5edb9cf6991b8aa6c
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 21:29:19 +0300

    feat: Displaying errors and repopulating fieldsvalidating the form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,6 +11,19 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Define a snippetCreateForm struct to represent the form data and validation
+// errors for the form fields. Note that all the struct fields are deliberately
+// exported (i.e. start with a capital letter). This is because struct fields
+// must be exported in order to be read by the html/template package when
+// rendering the template.
+type snippetCreateForm struct {
+    Title       string
+    Content     string
+    Expires     int
+    FieldErrors map[string]string
+}
+
+
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
  // Because httprouter matches the "/" path exactly, we can now remove the
@@ -68,12 +81,22 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     data := app.newTemplateData(r)
+
+    // Initialize a new createSnippetForm instance and pass it to the template.
+    // Notice how this is also a great opportunity to set any default or
+    // 'initial' values for the form --- here we set the initial value for the 
+    // snippet expiry to 365 days.
+    data.Form = snippetCreateForm{
+        Expires: 365,
+    }
+
     app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
+
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	// Limit the request body size to 4096 bytes
-	r.Body = http.MaxBytesReader(w, r.Body, 4096)
+	//r.Body = http.MaxBytesReader(w, r.Body, 4096)
   // First we call r.ParseForm() which adds any data in POST request bodies
     // to the r.PostForm map. This also works in the same way for PUT and PATCH
     // requests. If there are any errors, we use our app.ClientError() helper to 
@@ -84,10 +107,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
         return
     }
 
-    // Use the r.PostForm.Get() method to retrieve the title and content
-    // from the r.PostForm map.
-    title := r.PostForm.Get("title")
-    content := r.PostForm.Get("content")
+  
 	 // Retrieve the roles selected by the user.
 	 roles := r.PostForm["roles"]
 
@@ -105,44 +125,59 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
         app.clientError(w, http.StatusBadRequest)
         return
     }
-
+// Create an instance of the snippetCreateForm struct containing the values
+    // from the form and an empty map for any validation errors.
+    form := snippetCreateForm{
+        Title:       r.PostForm.Get("title"),
+        Content:     r.PostForm.Get("content"),
+        Expires:     expires,
+        FieldErrors: map[string]string{},
+    }
 	// Initialize a map to hold any validation errors for the form fields.
     fieldErrors := make(map[string]string)
 
     // Check that the title value is not blank and is not more than 100
     // characters long. If it fails either of those checks, add a message to the
     // errors map using the field name as the key.
-    if strings.TrimSpace(title) == "" {
-        fieldErrors["title"] = "This field cannot be blank"
-    } else if utf8.RuneCountInString(title) > 100 {
+    if strings.TrimSpace(form.Title) == "" {
+        form.FieldErrors["title"] = "This field cannot be blank"
+    } else if utf8.RuneCountInString(form.Title) > 100 {
         fieldErrors["title"] = "This field cannot be more than 100 characters long"
     }
 
     // Check that the Content value isn't blank.
-    if strings.TrimSpace(content) == "" {
-        fieldErrors["content"] = "This field cannot be blank"
+    if strings.TrimSpace(form.Content) == "" {
+        form.FieldErrors["content"] = "This field cannot be blank"
     }
 
     // Check the expires value matches one of the permitted values (1, 7 or
     // 365).
-    if expires != 1 && expires != 7 && expires != 365 {
-        fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+    if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+        form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
     }
 
-    // If there are any errors, dump them in a plain text HTTP response and
-    // return from the handler.
-    if len(fieldErrors) > 0 {
-        fmt.Fprint(w, fieldErrors)
+  // If there are any validation errors re-display the create.tmpl template,
+    // passing in the snippetCreateForm instance as dynamic data in the Form 
+    // field. Note that we use the HTTP status code 422 Unprocessable Entity 
+    // when sending the response to indicate that there was a validation error.
+    
+	if len(form.FieldErrors) > 0 {
+        data := app.newTemplateData(r)
+        data.Form = form
+        app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
         return
     }
 
-	
 
-    id, err := app.snippets.Insert(title, content, expires)
+
+    // We also need to update this line to pass the data from the
+    // snippetCreateForm instance to our Insert() method.
+    id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
     if err != nil {
         app.serverError(w, err)
         return
     }
+
 
     // Update the redirect path to use the new clean URL format.
     http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
