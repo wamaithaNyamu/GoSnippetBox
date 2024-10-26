c0ed4ada4afd4a417dd2019b01d18401bb1d69cb
Wamaitha <wamaithanyamu@gmail.com>
2024-10-26 17:31:00 +0300

    feat: added security headers as a middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,9 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that it returns a
+// http.Handler instead of *http.ServeMux.
+func (app *application) routes() http.Handler {
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +14,8 @@ func (app *application) routes() *http.ServeMux {
     mux.HandleFunc("/snippet/view", app.snippetView)
     mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-    return mux
+    // Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+    // Because secureHeaders is just a function, and the function returns a
+    // http.Handler we don't need to do anything else.
+    return secureHeaders(mux)
 }
