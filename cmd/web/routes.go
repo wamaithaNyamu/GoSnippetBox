7e29a5324d2f31d57c09a8715d37c4da5d5a0c1d
Wamaitha <wamaithanyamu@gmail.com>
2024-10-27 14:04:09 +0300

    feat: user authentication routes setup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -41,6 +41,12 @@ func (app *application) routes() http.Handler {
     router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
     router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
     router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+	// Add the five new routes, all of which use our 'dynamic' middleware chain.
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
 
 	//  Create a middleware chain containing our 'standard' middleware
